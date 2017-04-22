b36202a818c440284888e5bdedf1e6a985826b40
Jerry Ryle <jerry@mindtribe.com>
2017-04-21 23:42:43 -0700

    Tag filter working

--- js/tag_filter.js
+++ js/tag_filter.js
@@ -2,50 +2,110 @@
  * Copyright 2017 by Jerry Ryle
  */
 
-function TagFilter(entry_selector, entry_tag_selector, tag_class) {
-    var entries = $(entry_selector);
-    var entries_by_tag = {};
-    var tag_instances_by_tag = {};
+function TagFilter() {
+    this.items_by_tag = {};
+    this.all_items = [];
+    this.selected_tags = [];
+    this.unavailable_tags = [];
+    this.included_items = [];
+    this.excluded_items = [];
 
-    // Iterate over the specified entries, located the specified tags for each entry and
-    // gather a hash that maps tags to entries.
-    entries.each(function () {
-        var entry = $(this);
-        entry.find(entry_tag_selector).each(
-            function () {
-                var entry_tags = $(this);
-                var entry_tag_list = entry_tags.text().replace(/\s+/g, ' ').split(/,\s*/).map(function (element) {
-                    return element.trim();
-                }).sort(function (a, b) {
-                    return a.toLowerCase().localeCompare(b.toLowerCase());
-                });
-                entry_tags.empty();
-
-                entry_tag_list.forEach(function(element){
-                    if (!entries_by_tag.hasOwnProperty(element)) {
-                        entries_by_tag[element] = [];
-                    }
-                    entries_by_tag[element].push(entry);
-                    var clickable_tag_instance = $('<a>', {
-                        'class': tag_class,
-                        'text' : element,
-                        'href' : '#',
-                        'click': function(event) {
-                            event.preventDefault();
-                        }
-                    });
-                    entry_tags.append(clickable_tag_instance);
-
-                    if (!tag_instances_by_tag.hasOwnProperty(element)) {
-                        tag_instances_by_tag[element] = [];
-                    }
-                    tag_instances_by_tag[element].push(clickable_tag_instance);
-                });
-            }
-        );
-    });
-
-    this.entries_by_tag = entries_by_tag;
-    this.tag_instances_by_tag = tag_instances_by_tag;
-    console.log(this.tag_instances_by_tag);
+    this.most_items_for_tag = 0;
+    this.least_items_for_tag = 0;
 }
+
+TagFilter.prototype.addItemIDWithTags = function (item_id, tags) {
+    var obj_this = this;
+    tags.forEach(function (tag) {
+        if (!obj_this.items_by_tag.hasOwnProperty(tag)) {
+            obj_this.items_by_tag[tag] = [];
+        }
+        obj_this.items_by_tag[tag].push(item_id);
+    });
+    obj_this.all_items.push(item_id);
+};
+
+TagFilter.prototype.toggleTagSelection = function (tag) {
+    if (this.tagIsSelected(tag)) {
+        this.removeTagFromSelection(tag);
+    } else {
+        this.addTagToSelection(tag);
+    }
+};
+
+TagFilter.prototype.addTagToSelection = function (tag) {
+    if (!this.tagIsSelected(tag)) {
+        this.selected_tags.push(tag);
+    }
+};
+
+TagFilter.prototype.removeTagFromSelection = function (tag) {
+    var tag_selected_index = $.inArray(tag, this.selected_tags);
+
+    if (tag_selected_index >= 0) {
+        this.selected_tags.splice(tag_selected_index, 1);
+    }
+};
+
+TagFilter.prototype.clearSelection = function (tag) {
+    this.selected_tags = [];
+};
+
+TagFilter.prototype.updateFilter = function () {
+    var obj_this = this;
+
+    this.most_items_for_tag = _.reduce(this.items_by_tag, function (most, tag) {
+        return (tag.length > most) ? tag.length : most;
+    }, 0);
+    this.least_items_for_tag = _.reduce(this.items_by_tag, function (least, tag) {
+        return (tag.length < least) ? tag.length : least;
+    }, Number.MAX_VALUE);
+
+    if (this.selected_tags.length > 0) {
+        this.included_items = _.reduce(this.selected_tags, function (items, tag) {
+            return _.intersection(obj_this.items_by_tag[tag], items);
+        }, this.all_items);
+        this.excluded_items = _.difference(this.all_items, obj_this.included_items);
+        this.unavailable_tags = _.reduce(_.keys(this.items_by_tag), function (tags, tag) {
+            if (_.intersection(obj_this.items_by_tag[tag], obj_this.included_items).length <=0) {
+                tags.push(tag);
+            }
+            return tags;
+        }, []);
+    } else {
+        // No tags are selected, include all items.
+        this.included_items = this.all_items;
+        this.excluded_items = [];
+        this.unavailable_tags = [];
+    }
+};
+
+TagFilter.prototype.tagIsSelected = function (tag) {
+    return ($.inArray(tag, this.selected_tags) >= 0);
+};
+
+TagFilter.prototype.tagIsUnavailable = function (tag) {
+    return ($.inArray(tag, this.unavailable_tags) >= 0);
+};
+
+TagFilter.prototype.includedItems = function () {
+    return this.included_items;
+};
+
+TagFilter.prototype.excludedItems = function () {
+    return this.excluded_items;
+};
+
+TagFilter.prototype.mostItemsForTag = function () {
+    return this.most_items_for_tag;
+};
+
+TagFilter.prototype.leastItemsForTag = function () {
+    return this.least_items_for_tag;
+};
+
+TagFilter.prototype.allTags = function () {
+    return _.keys(this.items_by_tag).sort(function (a, b) {
+        return a.toLowerCase().localeCompare(b.toLowerCase());
+    })
+};
